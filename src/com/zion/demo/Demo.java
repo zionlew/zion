fbeb3cfcffd56d8822764a46b92084ffdb31478c
zionlew <zionlew@163.com>
2015-04-22 22:45:21 +0800

    环境搭建

---
+++ src/com/zion/demo/Demo.java
@@ -0,0 +1,16 @@
+/**----------------------------------------------------------+
+ +   PGM-ID  :   Demo.java
+ +   Author  :   H.Liu
+ +   Date    :   2015年4月22日 下午10:44:45
+ +   Function:   
+ +               
+ +-----------------------------------------------------------*/
+package com.zion.demo;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
